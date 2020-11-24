71037a830b0546c5f65c5b4115d296362471f049
remy moulinier <rmoulinier@outlook.com>
2020-11-24 18:44:08 +0100

    added things

--- app/controllers/lost_people_controller.rb
+++ app/controllers/lost_people_controller.rb
@@ -1,10 +1,33 @@
 class LostPeopleController < ApplicationController
-  # def index
-  #   @restaurants = policy_scope(LostPerson)
-  # end
+  def index
+    # @restaurants = policy_scope(LostPerson)
+  end
 
   def new
     @lost_person = LostPerson.new
     authorize @lost_person
+  end
+
+  def create
+    @lost_person = LostPerson.new(lost_person_params)
+    authorize @lost_person
+    @lost_person.user = current_user
+    if @lost_person.save
+      redirect_to lost_person_path(@lost_person)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @lost_person = LostPerson.find(params[:id])
+    authorize @lost_person
+  end
+
+  private
+
+  def lost_person_params
+    params.require(:lost_person).permit(:first_name, :last_name, :age, :last_known_location, :last_known_clothes,
+                                        :height, :body_type, :description, :disparition_date_time, :photo)
   end
 end
