0578dfcea1b2e745e863ee84667f4345052bb2cd
Noemi Valin <noemivalin@outlook.fr>
2020-11-24 18:44:55 +0100

    a finir

--- app/controllers/search_parties_controller.rb
+++ app/controllers/search_parties_controller.rb
@@ -4,13 +4,17 @@ class SearchPartiesController < ApplicationController
   #   @searchparties = policy_scope(SearchParty)
   # end
 
-  # def new
-  #   authorize @search_party
-  # end
+  def new
+    authorize @search_party
+    @search_party = SearchParty.new
+  end
 
-  # def create
-  #   authorize @search_party
-  # end
+  def create
+    authorize @search_party
+    @search_party = SearchParty.new(search_party_params)
+    @search_party.save 
+    redirect_to search_party_path(search_party)
+  end
 
   # def edit
   #   authorize @search_party
@@ -27,9 +31,9 @@ class SearchPartiesController < ApplicationController
   #   redirect_to lost_people_path
   # end
 
-#   private
+  private
 
-#   def search_party_params
-#     params.require(:searchparty).permit(:start_date_time, :end_date_time, :meeting_location, :description, :radius)
-#   end
-# end
+  def search_party_params
+    params.require(:search_party).permit(:start_date_time, :end_date_time, :meeting_location, :description, :radius)
+  end
+end
