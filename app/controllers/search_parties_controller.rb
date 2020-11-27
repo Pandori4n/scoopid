ab8bdde066e2847294b19d7b702b99595fbc5b9e
Aymeric Faidherbe <aymeric.faidherbe@gmail.com>
2020-11-27 14:49:00 +0100

    Search Party Index

--- app/controllers/search_parties_controller.rb
+++ app/controllers/search_parties_controller.rb
@@ -15,20 +15,16 @@ class SearchPartiesController < ApplicationController
   end
 
   def create
+    @search_party = SearchParty.new(search_party_params)
     authorize @search_party
-  end
-
-  # def edit
-    # @search_party = SearchParty.new(search_party_params)
-    # authorize @search_party
-    # @search_party.lost_person = current_user.lost_people.first
+    @search_party.lost_person = current_user.lost_people.first
     # @search_party.user = current_user
-    # if @search_party.save
-      # redirect_to search_party_path(@search_party)
-    # else
-      # render :new
-    # end
-  # end
+    if @search_party.save
+      redirect_to search_party_path(@search_party)
+    else
+      render :new
+    end
+  end
 
   def edit
     @search_party = SearchParty.find(params[:id])
