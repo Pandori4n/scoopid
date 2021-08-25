9e416d344b026fe4c029bbc434466ad307d28ea9
Basile Rieu <basilerieu@yahoo.fr>
2021-08-25 12:34:56 +0200

    Simple Authentication gem installed

--- Gemfile
+++ Gemfile
@@ -30,6 +30,8 @@ gem 'bootsnap', '>= 1.4.2', require: false
 
 gem 'devise'
 
+gem 'simple_token_authentication', '~> 1.0'
+
 # Authorization
 gem 'pundit'
 # Gemfile
--- app/models/lost_person.rb
+++ app/models/lost_person.rb
@@ -1,6 +1,18 @@
 require 'securerandom'
 
 class LostPerson < ApplicationRecord
+  acts_as_token_authenticatable
+
+  # Note: you can include any module you want. If available,
+  # token authentication will be performed before any other
+  # Devise authentication method.
+  #
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :lockable
+
   belongs_to :user
 
   has_many :search_parties
@@ -20,6 +32,6 @@ class LostPerson < ApplicationRecord
   private
 
   def generate_code
-    self.code = SecureRandom.alphanumeric(5)
+    self.code = SecureRandom.alphanumeric(7)
   end
 end
